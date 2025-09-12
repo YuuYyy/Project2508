d5a79f203073a4dfc981489f08eedcf28bdc430c
xxj <2441955789@qq.com>
2025-09-12 12:13:22 +0800

    完善登录注册

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -4,8 +4,9 @@ import RecordIndexView from '@/views/record/RecordIndexView'
 import RanklistIndexView from '@/views/ranklist/RanklistIndexView'
 import UserBotIndexView from '@/views/user/bot/UserBotIndexView'
 import NotFound from '@/views/error/NotFound'
-import UserAccountLoginView from '@/views/user/account/UserAccountLoginView.vue'
-import UserAcccountRegisterView from '@/views/user/account/UserAcccountRegisterView.vue'
+import UserAccountLoginView from '@/views/user/account/UserAccountLoginView'
+import UserAcccountRegisterView from '@/views/user/account/UserAcccountRegisterView'
+import store from '@/store/index'
 
 //将地址与组件映射
 //输入网址后，从上至下匹配
@@ -14,41 +15,65 @@ const routes = [
     path: "/",
     name: "home",
     redirect: "/pk/",
+    meta: {
+      requestAuth: true,  // 该页面需要授权
+    }
   },
   {
     path: "/pk/",
     name: "pk_index",
     component: PkIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/record/",
     name: "record_index",
     component: RecordIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
    {
     path: "/ranklist/",
     name: "ranklist_index",
     component: RanklistIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/user/bot/",
     name: "user_bot_index",
     component: UserBotIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/user/account/login/",
     name: "user_account_login",
     component: UserAccountLoginView,
+    meta: {
+      requestAuth: false,
+    }
   },
   {
     path: "/user/account/register/",
     name: "user_account_register",
     component: UserAcccountRegisterView,
+    meta: {
+      requestAuth: false,
+    }
   },
   {
     path: "/404/",
     name: "404",
     component: NotFound,
+    meta: {
+      requestAuth: false,
+    }
   },
   {
     path: "/:catchAll(.*)", //匹配所有字符
@@ -59,6 +84,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.requestAuth && !store.state.user.is_login) { // 如果目标页面需要授权且当前未登录，则重定向到登录页面
+    next({name: "user_account_login"});
+  } else {
+    next();
+  }
 })
 
 export default router
